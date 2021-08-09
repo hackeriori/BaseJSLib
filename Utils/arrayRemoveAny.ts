712c2940cb68d35021e6e2d4e0187c398f2e138c
Administrator <Administrator@DESKTOP-OKRG5S0>
2021-08-09 14:33:08 +0800

    可以只删除一个

--- Utils/arrayRemoveAny.ts
+++ Utils/arrayRemoveAny.ts
@@ -2,6 +2,7 @@
  * 移除数组中匹配表达式的所有项
  * @param array 数组
  * @param fun 表达式
+ * @param firstOnly 是否只删除第一个匹配项
  */
 export function arrayRemoveAny<T>(array: T[], fun: (item: T) => boolean, firstOnly = false) {
   for (let i = array.length - 1; i >= 0; i--) {
