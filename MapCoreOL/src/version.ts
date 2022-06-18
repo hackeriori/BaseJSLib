1bbe9a6c12515fc4eb6cc9a677dae9e5748129b1
Administrator <Administrator@DESKTOP-OKRG5S0>
2022-06-18 16:56:04 +0800

    取消top避免跨域问题

--- MapCoreOL/src/version.ts
+++ MapCoreOL/src/version.ts
@@ -1,5 +1,5 @@
 export default function setVersion() {
-  if (!window.top.versionInfo)
-    window.top.versionInfo = {};
-  window.top.versionInfo[process.env.projName!] = process.env.version!;
+  if (!window.versionInfo)
+    window.versionInfo = {};
+  window.versionInfo[process.env.projName!] = process.env.version!;
 }
